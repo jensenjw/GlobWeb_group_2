47eac1a126fc403f1234dd02f205ebcf3c4c353e
Markus Løtveit <69514021+h593277@users.noreply.github.com>
2022-09-28 13:19:58 +0200

    Initial version of cart servlet and jsp

--- src/main/java/controllers/CartServlet.java
+++ src/main/java/controllers/CartServlet.java
@@ -1,12 +1,15 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Product;
 
 @WebServlet("/cart")
 public class CartServlet extends HttpServlet {
@@ -15,13 +18,17 @@ public class CartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    	
+    
     	
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+    	List<Product> products = (List<Product>) request.getAttribute("cartProducts");
+    	Product product = (Product) request.getAttribute("remove");
+    	products.remove(product);
+    	request.setAttribute("cartProducts", products);
     	
     	response.sendRedirect("cartServlet");
     }
--- src/main/webapp/cart.jsp
+++ src/main/webapp/cart.jsp
@@ -17,6 +17,21 @@
 		</h1>
 
 	</fmt:bundle>
-
+	<table>
+	<fmt:foreach var=product items="${yourProducts}">
+	<tr>
+	<td>${product.pNo}</td>
+	<td>${product.pName}</td>
+	<td>${product.priceInEuro}</td>
+	<td><img src=${product.imageFile} alt=${product.pName}></td>
+	<td>${product.pDescription}</td>
+	<td>
+	<form action=cart method="post">
+	<button name="remove" value="${product}" type="submit"></button>
+	</form>
+	</td>
+	</tr>
+	</fmt:foreach>
+</table>
 </body>
 </html>
