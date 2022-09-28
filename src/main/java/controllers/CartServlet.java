b772a0aa9fe7aada0177d90d476b119bdf649d84
JV-Jones <572018@stud.hvl.no>
2022-09-28 11:50:08 +0200

    Product and Cart servlets

---
+++ src/main/java/controllers/CartServlet.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/cart.jsp")
+public class CartServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	
+    	
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+}
